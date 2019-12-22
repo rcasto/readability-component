ac7b53b0c5654c19d689125e7289da3399472a73
Richie Casto <rcasto92@gmail.com>
2019-12-22 18:52:05 -0500

    add dist folder, built

---
+++ dist/readability.js
@@ -0,0 +1,156 @@
+(function () {
+    'use strict';
+
+    const vowels = new Set([
+        'a',
+        'e',
+        'i',
+        'o',
+        'u'
+    ]);
+    const diphthongs = new Set([
+        'ee',
+        'ea',
+        'oo',
+        'ou',
+        'ow',
+        'ie',
+        'ay',
+        'oi',
+    ]);
+    const triphthongs = new Set([
+        'eau',
+        'iou',
+        'our',
+        'ire',
+        'ier'
+    ]);
+
+    // words per minute
+    const averageReadingSpeed = 180;
+
+    function getAverageTimeToRead(text) {
+        return (text || '').length / averageReadingSpeed;
+    }
+
+    function getReadability(text) {
+        text = normalizeText(text);
+
+        const numWords = getNumWords(text);
+        const numSentences = getNumSentences(text);
+        const numSyllables = getNumSyllablesFromText(text);
+
+        const readability = 206.835 - 1.015 * (numWords / numSentences) - 84.6 * (numSyllables / numWords);
+        return getReadabilityMapping(readability);
+    }
+
+    function getReadabilityMapping(readability) {
+        if (readability >= 90) {
+            return 'Very easy to read';
+        } else if (readability < 90 && readability >= 80) {
+            return 'Easy to read';
+        } else if (readability < 80 && readability >= 70) {
+            return 'Fairly easy to read';
+        } else if (readability < 70 && readability >= 60) {
+            return 'Plain English';
+        } else if (readability < 60 && readability >= 50) {
+            return 'Fairly difficult to read';
+        } else if (readability < 50 && readability >= 30) {
+            return 'Difficult to read';
+        } else {
+            return 'Very difficult to read';
+        }
+    }
+
+    function getNumWords(text, wordDelimiter = ' ') {
+        return text
+            .split(wordDelimiter).length;
+    }
+
+    function getNumSentences(text, sentenceDelimiter = '.') {
+        return text
+            .split(sentenceDelimiter).length;
+    }
+
+    function getNumSyllablesFromText(text, wordDelimiter = ' ') {
+        const words = text.split(wordDelimiter);
+        return words.reduce((numSyllables, word) => {
+            return numSyllables + getNumSyllables(word);
+        }, 0);
+    }
+
+    function getNumSyllables(word) {
+        const wordLength = (word || '').length;
+        let numSyllables = 0;
+        let tmpStr;
+
+        for (let i = 0; i < wordLength; i++) {
+            if (vowels.has(word[i])) {
+                numSyllables++;
+            }
+            if (i + 1 < wordLength) {
+                tmpStr = word.substring(i, i + 2);
+                if (diphthongs.has(tmpStr)) {
+                    numSyllables--;
+                }
+            }
+            if (i + 2 < wordLength) {
+                tmpStr = word.substring(i, i + 3);
+                if (triphthongs.has(tmpStr)) {
+                    numSyllables--;
+                }
+            }
+        }
+
+        return numSyllables;
+    }
+
+    function normalizeText(text) {
+        return (text || '')
+            .trim()
+            .toLowerCase();
+    }
+
+    const templateContent = `
+  <style>
+    .readability {
+      display: flex;
+      flex-direction: column;
+    }
+    .readability * {
+      margin-left: auto;
+    }
+  </style>
+  <slot name="readable-text"></slot>
+`;
+
+    const template = document.createElement('template');
+    template.innerHTML = templateContent;
+
+    class Readability extends HTMLElement {
+      constructor() {
+        super();
+
+        const templateContent = template.content;
+        const shadowRoot = this.attachShadow({
+          mode: 'open'
+        });
+        const readabilityDiv = document.createElement('div');
+        const readabilityLevelDiv = document.createElement('div');
+        const readabilityAverageReadingTimeDiv = document.createElement('div');
+
+        readabilityLevelDiv.textContent = getReadability(this.textContent);
+        readabilityAverageReadingTimeDiv.textContent = `${getAverageTimeToRead(this.textContent)} minutes to read`;
+
+        readabilityDiv.className = 'readability';
+        readabilityDiv.appendChild(readabilityLevelDiv);
+        readabilityDiv.appendChild(readabilityAverageReadingTimeDiv);
+
+        shadowRoot.appendChild(templateContent.cloneNode(true));
+        shadowRoot.appendChild(readabilityDiv);
+      }
+    }
+
+    customElements.define('read-ability', Readability);
+
+}());
