ba8600ca6173d98871baad93116bbe104ecea675
Richie Casto <rcasto92@gmail.com>
2020-05-09 18:24:40 -0700

    add @rollup/plugin-json plugin

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,10 +1,14 @@
 import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
 
 const config = {
 	input: 'src/readability.js',
-	plugins: [babel({
-		babelHelpers: 'bundled'
-	})],
+	plugins: [
+		babel({
+			babelHelpers: 'bundled'
+		}),
+		json()
+	],
 };
 
 export default config;
