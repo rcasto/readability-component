7861b6b358c86143ed75abfcf30269c80d3a1d27
Richie Casto <rcasto92@gmail.com>
2020-05-09 18:57:43 -0700

    update generator script to use current heuristic, generate dictionary for incorrect

--- data/syllableCountMapGenerator.js
+++ data/syllableCountMapGenerator.js
@@ -9,6 +9,55 @@ const mobyHyphenationListFilePath = path.resolve(__dirname, './moby-hyphenation-
 const outputFilePath = path.resolve(__dirname, './syllableCount.json');
 const hyphenDelimiterRegex = /¥/g;
 
+const vowelRegex = /[aeiouy]+/g;
+
+let numWords = 0;
+let numCorrect = 0;
+
+function prettyPrint(obj) {
+    return JSON.stringify(obj, null, 2);
+}
+
+function getNumSyllables(word) {
+    word = word || '';
+
+    const wordLength = word.length;
+    let numSyllables = 0;
+
+    // substitute 'a' as the universal vowel
+    // replacing consecutive vowels with a single occurrence
+    const vowelReplacedWord =
+        word.replace(vowelRegex, 'a');
+    const vowelReplacedWordLength = vowelReplacedWord.length;
+
+    for (let i = 0; i < vowelReplacedWordLength; i++) {
+        if (vowelReplacedWord[i] === 'a') {
+            numSyllables++;
+        }
+    }
+
+    // At this point, numSyllables essentially
+    // is equivalent to the number of consolidated vowels
+
+    // remove silent 'e' heuristic
+    if (
+        numSyllables >= 2 &&
+        word[wordLength - 1] === 'e'
+    ) {
+        return getNumSyllables(word.slice(0, wordLength - 1));
+    }
+    // remove silent 'ed' heuristic
+    else if (
+        numSyllables >= 2 &&
+        word[wordLength - 2] === 'e' &&
+        word[wordLength - 1] === 'd'
+    ) {
+        return getNumSyllables(word.slice(0, wordLength - 2));
+    }
+
+    return numSyllables;
+}
+
 fsReadFileAsync(mobyHyphenationListFilePath, {
     encoding: 'utf8'
 })
@@ -18,8 +67,29 @@ fsReadFileAsync(mobyHyphenationListFilePath, {
     const wordTokens = word.trim().split(' ');
     const wordKey = wordTokens.join('').toLowerCase();
     const numSyllables = wordTokens.length;
-    wordMap[wordKey] = numSyllables;
+    const numCalculatedSyllabled = getNumSyllables(wordKey);
+
+    // filter out words with hyphens in them
+    // don't include them in accuracy check either
+    //
+    // This is mainly another way to try and trim the data size
+    // slightly
+    if (wordKey.includes('-')) {
+        return wordMap;
+    }
+
+    // filter out words that the heuristic is correct on
+    // but use them for accuracy calculation
+    if (numSyllables === numCalculatedSyllabled) {
+        numCorrect++;
+    } else {
+        wordMap[wordKey] = numSyllables;
+    }
+
+    numWords++;
+
     return wordMap;
 }, {}))
-.then(words => fsWriteFileAsync(outputFilePath, JSON.stringify(words), 'utf8'))
+.then(words => fsWriteFileAsync(outputFilePath, prettyPrint(words), 'utf8'))
+.then(() => console.log(numCorrect, numWords - numCorrect, numWords, numCorrect / numWords * 100))
 .catch(err => console.error(err));
